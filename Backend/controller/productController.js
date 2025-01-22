48fce4acd5c8a58f6cb7d52294b3b73fafbf108d
Rajeev Yadav <143483622+RajeevYadav1999@users.noreply.github.com>
2025-01-22 23:10:13 +0530

    Added Filter Functionality to Browse Products Using Category

--- Backend/controller/productController.js
+++ Backend/controller/productController.js
@@ -15,7 +15,7 @@ export const createProducts=handleAsyncError (async(req,res,next)=>{
 
 // 2️⃣ getting all products
 export const getAllProducts =handleAsyncError (async(req, res, next)=>{
-  const apiFunctionality= new APIFunctionality(Product.find(),req.query).search();
+  const apiFunctionality= new APIFunctionality(Product.find(),req.query).search().filter();
   const products= await apiFunctionality.query
     res.status(200).json({
         success: true,
--- Backend/utils/apiFunctionality.js
+++ Backend/utils/apiFunctionality.js
@@ -13,6 +13,14 @@ class APIFunctionality{
         this.query=this.query.find({...keyword});
         return this
     }
+
+    filter(){
+        const queryCopy = {...this.queryStr}
+        const removeFields=['keyword','page','limit']
+        removeFields.forEach(key=>delete queryCopy[key])
+        this.query=this.query.find(queryCopy)
+        return this
+    }
 }
 
 export default APIFunctionality;
