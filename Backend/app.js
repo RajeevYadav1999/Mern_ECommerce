b66f27a52ecba7c7c311a2ed6fe08d9256fd986e
Rajeev Yadav <143483622+RajeevYadav1999@users.noreply.github.com>
2025-02-01 23:03:35 +0530

    Added Register User Functionality

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import product from './routes/productRoutes.js'
+import user from './routes/userRoutes.js'
 import errorHandleMiddleware from './middleware/error.js'
 const app = express();
 
@@ -7,7 +8,8 @@ const app = express();
 app.use(express.json())
 
 //Routes
-app.use('/api/v1',product) //middleware for creating the route
+app.use('/api/v1',product) //middleware for creating route
+app.use('/api/v1',user) 
 
 app.use(errorHandleMiddleware)
 export default app;
