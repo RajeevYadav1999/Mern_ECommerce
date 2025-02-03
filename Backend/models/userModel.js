6139680af7ba9d941244968cf061b1f6c2e97362
Rajeev Yadav <143483622+RajeevYadav1999@users.noreply.github.com>
2025-02-03 22:53:04 +0530

    Verifying Password Using Bcryptjs

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -55,5 +55,8 @@ userSchema.methods.getJWTToken=function(){
         expiresIn:process.env.JWT_EXPIRE
     })
 }
+userSchema.methods.verifyPassword=async function(userEnteredPassword){
+    return await bcryptjs.compare(String(userEnteredPassword), this.password);
+}
 
 export default mongoose.model("User",userSchema);
