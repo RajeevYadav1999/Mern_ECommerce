19ce097e906bfbfb0a53798bce122968c253d952
Rajeev Yadav <143483622+RajeevYadav1999@users.noreply.github.com>
2025-02-05 23:25:21 +0530

    Added Logout User Functionality

--- Backend/controller/userController.js
+++ Backend/controller/userController.js
@@ -34,3 +34,16 @@ export const loginUser=handleAsyncError(async(req,res,next)=>{
    }
    sendToken(user,200,res)
 })
+
+//! Logout
+
+export const logout=handleAsyncError(async(req,res,next)=>{
+    res.cookie('token',null,{
+        expires:new Date(Date.now()),
+        httpOnly:true
+    })
+    res.status(200).json({
+        success:true,
+        message:"Successfully Logged Out"
+    })
+})
